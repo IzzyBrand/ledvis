419a4e911fd6636b2a8ff6fd85dee1c0b3e1e8d7
Izzy Brand <isaiah_brand@brown.edu>
2018-11-08 20:21:54 -0500

    reorganized

--- config.py
+++ config.py
@@ -6,14 +6,23 @@ SAMPLING_FREQ	= 44100  # Sampling frequency of incoming audio
 DEVICE_INDEX 	= 2      # Which audio device to read from (listed in pyaudio_test.py)
 
 # LED CONFIGURATION
-LED_COUNT      = 10      # Number of LED pixels per column
-LED_PIN        = 18      # GPIO pin connected to the pixels (18 uses PWM!).
-LED_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
-LED_DMA        = 10      # DMA channel to use for generating signal (try 10)
-LED_BRIGHTNESS = 128     # Set to 0 for darkest and 255 for brightest
-LED_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
-LED_CHANNEL    = 0       # set to '1' for GPIOs 13, 19, 41, 45 or 53
+LED_1_COUNT      = 150      # Number of LED pixels.
+LED_1_PIN        = 18      # GPIO pin connected to the pixels (must support PWM! GPIO 13 and 18 on RPi 3).
+LED_1_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_1_DMA        = 10      # DMA channel to use for generating signal (Between 1 and 14)
+LED_1_BRIGHTNESS = 25     # Set to 0 for darkest and 255 for brightest
+LED_1_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_1_CHANNEL    = 0       # 0 or 1
+
+LED_2_COUNT      = 150      # Number of LED pixels.
+LED_2_PIN        = 13      # GPIO pin connected to the pixels (must support PWM! GPIO 13 or 18 on RPi 3).
+LED_2_FREQ_HZ    = 800000  # LED signal frequency in hertz (usually 800khz)
+LED_2_DMA        = 11      # DMA channel to use for generating signal (Between 1 and 14)
+LED_2_BRIGHTNESS = 25     # Set to 0 for darkest and 255 for brightest
+LED_2_INVERT     = False   # True to invert the signal (when using NPN transistor level shift)
+LED_2_CHANNEL    = 1       # 0 or 1
+
 
 # SERVER CONFIGURATION
-SERVER_ADDRESS = 'izzypi.local'
+SERVER_ADDRESS = 'izzypi'
 SERVER_PORT    = 10000
--- simple_voo.py
+++ simple_voo.py
@@ -8,10 +8,14 @@ NUM_SAMPLES = 5
 
 # Create an ADC object
 adc = Adafruit_ADS1x15.ADS1015()
+adc.start_adc(0, gain=GAIN)
 # Create NeoPixel object with appropriate configuration.
-strip = Adafruit_NeoPixel(LED_COUNT, LED_PIN, LED_FREQ_HZ, LED_DMA, LED_INVERT, LED_BRIGHTNESS, LED_CHANNEL)
+strip1 = Adafruit_NeoPixel(LED_1_COUNT, LED_1_PIN, LED_1_FREQ_HZ, LED_1_DMA, LED_1_INVERT, LED_1_BRIGHTNESS, LED_1_CHANNEL)
+strip2 = Adafruit_NeoPixel(LED_2_COUNT, LED_2_PIN, LED_2_FREQ_HZ, LED_2_DMA, LED_2_INVERT, LED_2_BRIGHTNESS, LED_2_CHANNEL)
+
 # Intialize the library (must be called once before other functions).
-strip.begin()
+strip1.begin()
+strip2.begin()
 
 color = Color(50, 100, 200)
 off = Color(0,0,0)
@@ -20,15 +24,19 @@ max_val = 0
 while True:
     abs_val = 0
     for i in range(NUM_SAMPLES):
-        abs_val += abs(adc.read_adc_difference(0, gain=GAIN))
-    abs_val = abs_val/NUM_SAMPLES
+        abs_val += abs(adc.get_last_result())
+    abs_val = abs_val/float(NUM_SAMPLES)
 
     max_val = max(max_val, abs_val)
 
-    num_leds_on = LED_COUNT * abs_val/max_val
+    num_leds_on = LED_1_COUNT * abs_val/max_val
 
-    for i in range(LED_COUNT):
-        strip.setPixelColor(i, color if i < num_leds_on else off)
+    for i in range(LED_1_COUNT):
+        strip1.setPixelColor(i, color if i < num_leds_on else off)
+        strip2.setPixelColor(i, color if i < num_leds_on else off)
 
-    strip.show()
+    strip1.show()
+    time.sleep(0.005)
+    strip2.show()
+    time.sleep(0.005)
 
