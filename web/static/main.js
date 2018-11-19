e46ab22bd3a42bf2a70e35269f7d3bbace107b7e
Izzy Brand <isaiah_brand@brown.edu>
2018-11-19 00:00:30 -0500

    tried to add a slider

--- web/static/main.js
+++ web/static/main.js
@@ -1,3 +1,19 @@
+// $(document).ready(function(){
+
+// 	console.log("adding slider");
+// 	var slider = document.getElementById("brightnessSlider");
+
+// 	slider.addEventListener('mouseup', function() {
+// 		console.log(slider.value);
+// 		$.ajax({
+// 			'url': '/brightness_slider',
+// 			'data': {
+// 				'brightness': slider.value
+// 			}
+// 		});
+// 	});
+// });
+
 function btnClick(x, index) {
 	btn = $(x);
 	$.ajax({
